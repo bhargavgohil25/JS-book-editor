a4478859b925c8a0dbca7d4c548a5f8e9e241c87
Bhargav Gohil <bhargavgohil25@gmail.com>
2021-01-27 14:05:35 +0530

    triggering esbuild wasm for transpiling and bundling the code

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,41 @@
+import * as esbuild from 'esbuild-wasm'
+import { useState,useEffect } from "react";
 import ReactDOM from 'react-dom'
 
+
+
 const App = () => {
+    const [input,setInput] = useState('');
+    const [code,setCode] = useState('');
+    
+    // this service will take the code that is supposed to be transpiled and bundled
+    // esbuild will do the both the task in very rapid way... and therefore it used in here 
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker :  true,
+            wasmURL : '/esbuild.wasm'
+        });
+        console.log(service);
+    };
+    
+    useEffect(() => {
+        startService();
+    },[]);
+
+    const onClick = () => {
+        console.log(input);
+    };
+
     return (
-        <h1>Hi There !</h1>
+        <div>
+            <textarea value = {input} onChange = {(e) => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={onClick}>
+                    Submit
+                </button>
+            </div>
+            <pre>{code}</pre>
+        </div>
     );
 }
 
